545c0b307a4ca6a5eb31895bc023c4b7d1929650
andres <directorit@valleygroups.com>
2024-04-15 21:06:21 -0500

    Organizo la estructura del proyecto e inicio a crear la vista inicial del panel

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './web/home/home.component';
+import { DashboardComponent } from './panel/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'panel',
+        component: DashboardComponent
+    }
+];
---
+++ src/app/panel/dashboard/dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../components/header/header.component';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [
+    HeaderComponent,
+  ],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
+})
+export class DashboardComponent {
+
+}
