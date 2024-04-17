ac1331d9b5e8f6be8abf9395d10ce32921ecba07
Santiagoagp <73494281+Santiagoagp@users.noreply.github.com>
2024-04-16 20:57:02 -0500

    añadiendo vista de roles

--- src/app/panel/roles/roles.component.ts
+++ src/app/panel/roles/roles.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../components/header/header.component';
 
 @Component({
   selector: 'app-roles',
   standalone: true,
-  imports: [],
+  imports: [
+    HeaderComponent,
+  ],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
