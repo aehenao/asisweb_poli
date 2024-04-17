0007f31e3726e2f95f9534fa42466dc27b657eac
andres <directorit@valleygroups.com>
2024-04-16 20:56:42 -0500

    Realizo ajustes de links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,15 +3,13 @@ import { HomeComponent } from './web/home/home.component';
 import { DashboardComponent } from './panel/dashboard/dashboard.component';
 import { LoginComponent } from './web/login/login.component';
 import { RegisterComponent } from './web/register/register.component';
+import { UsersComponent } from './panel/users/users.component';
+import { RolesComponent } from './panel/roles/roles.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent
-    },
-    {
-        path: 'panel',
-        component: DashboardComponent
     },
     {
         path: 'login',
@@ -20,6 +18,23 @@ export const routes: Routes = [
     {
         path: 'registro',
         component: RegisterComponent
+    },
+    {
+        path: 'panel',
+        children: [
+            {
+                path: 'dashboard',
+                component: DashboardComponent
+            },
+            {
+                path: 'users',
+                component: UsersComponent
+            },
+            {
+                path: 'roles',
+                component: RolesComponent
+            }
+        ]
     },
    
 ];
