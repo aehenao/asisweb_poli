e85caa62b4cc3a17ff1957e69a412da70dcedc9e
andres <directorit@valleygroups.com>
2024-04-30 22:01:15 -0500

    Agrego función para validación de formulario en el login

--- src/app/web/login/login.component.ts
+++ src/app/web/login/login.component.ts
@@ -1,17 +1,37 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink, Router } from '@angular/router';
+import { FormControl, ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
     RouterLink,
+    ReactiveFormsModule,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  loginForm = this.fb.group({
+    email: new FormControl('admin@gmail.com', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  })
+
+  constructor(private fb: FormBuilder, private router: Router){}
+
+  ngOnInit() {
+  }
+
+  login() {
+    if(this.loginForm.valid) {  
+      console.log(this.loginForm.value);
+      this.router.navigateByUrl('/panel/dashboard')
+    }else{
+      alert("Faltan campos por rellenar");
+    }
+  }
 }
 
